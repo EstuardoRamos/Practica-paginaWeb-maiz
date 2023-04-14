9cd55b35e4268df863f895612a755bad626539f6
EstuardoRamos <estuardoramosg@gmail.com>
2023-04-14 11:11:56 -0600

    error en carpetas

--- pagina-web-maiz/pagina-web-maiz/controler/mostrar_fav.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include 'conection.php';
-	$sql = "SELECT color_favorito, COUNT(*) AS cantidad FROM maiz_favorito GROUP BY color_favorito";
-    $resultado = mysqli_query($conexion, $sql);
-    
-?>
